e19e1007ce729850498f59c1ea337d485afc2e30
Milind Mishra <milind.mishra4@gmail.com>
2024-03-27 23:17:05 +0530

    [NEW] minor changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>UI Library</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "UI Components Library",
   },
-}
+};
 
-export default config
+export default config;
